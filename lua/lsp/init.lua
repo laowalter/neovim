6aa2d46d1b839350be495a8986bb51b89b63e602
Laowalter <fffregister@gmail.com>
2021-05-06 18:31:32 +0800

    add vim.o.completeopt = "menuone,noselect" vim.o.shortmess = vim.o.shortmess .. "c"

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,9 +1,11 @@
+vim.o.shortmess = vim.o.shortmess .. "c"
+vim.o.completeopt = "menuone,noselect"
 require'lspconfig'.pyright.setup{on_attach=require'completion'.on_attach}
 require'lspconfig'.gopls.setup{
     on_attach= require'completion'.on_attach;
     settings = {
-        completeopt={menuone,noinsert,noselect};
-        shortmess={c};
+        --completeopt={menuone,noinsert,noselect};
+        --vim.o.shortmess=vim.o.shortmess .. c;
     };
     root_dir = require'lspconfig'.util.root_pattern(".git", "go", "gomod")
 }
