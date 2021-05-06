5c2f91dccbe7a965415f0f4ff5a615d68e1df365
Laowalter <fffregister@gmail.com>
2021-05-06 09:53:30 +0800

    telescope.vim added

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- Map leader to space
+vim.g.mapleader = ' '
+
+local fn = vim.fn
+local execute = vim.api.nvim_command
+
+-- Sensible defaults
+require('settings')
+
+-- Key mappings
+require('keymappings')
+
+-- Plugin
+require('plugins')
+
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
